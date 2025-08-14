08824fe8f79a9ef9ae722fc0501f3e11aac56906
Facundo Castillo <ismaelcastilloleites2013@hotmail.com>
2025-08-14 19:04:41 -0300

    Contraseña debe ser mayor a 6 caracteres

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,13 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+document.getElementById("regBtn").addEventListener("click", function() {
+
+    let password1 = document.getElementById("password1").value;
+
+     if (password1.length < 6) {
+    showAlertError(); 
+    return;
+  }})
