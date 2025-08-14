ef564d13deedad97075a48b1a234877b2fe34270
rodrigovivesuy <rodrigovivesuy@gmail.com>
2025-08-14 19:33:22 -0300

    Agrego validacion para que los campos no puedan estar vacios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const email = document.getElementById('email');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const terminosCheckbox = document.getElementById('terminos');
+const registrarBtn = document.getElementById('regBtn');
+
+if (nombre === '' ||
+    apellido === '' ||
+    email === '' ||
+    password1 === '' ||
+    password2 === '') {
+    showAlertError();
+    return;
+}
